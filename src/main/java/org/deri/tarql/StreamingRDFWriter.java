9a63a46afb7636e0f12b59ecc5c78f65d43d73bf
Richard Cyganiak <richard@cyganiak.de>
2016-12-24 10:57:01 +0000

    Ensure rdf: prefix is registered when writing Turtle (#36)

--- src/main/java/org/deri/tarql/StreamingRDFWriter.java
+++ src/main/java/org/deri/tarql/StreamingRDFWriter.java
@@ -11,6 +11,8 @@ import org.apache.jena.riot.system.StreamRDF;
 import org.apache.jena.riot.writer.WriterStreamRDFBlocks;
 import org.apache.jena.riot.writer.WriterStreamRDFPlain;
 import org.apache.jena.shared.PrefixMapping;
+import org.apache.jena.shared.impl.PrefixMappingImpl;
+import org.apache.jena.vocabulary.RDF;
 
 
 /**
@@ -40,6 +42,9 @@ public class StreamingRDFWriter {
 	}
 
 	public void writeTurtle(String baseIRI, PrefixMapping prefixes) {
+		// Auto-register RDF prefix so that rdf:type is displayed well
+		// All other prefixes come from the query and should be as author intended
+		prefixes = ensureRDFPrefix(prefixes);
 		WriterStreamRDFBlocks writer = new WriterStreamRDFBlocks(out);
 		writer.start();
 		writer.base(baseIRI);
@@ -48,5 +53,17 @@ public class StreamingRDFWriter {
 		}
 		StreamOps.sendTriplesToStream(triples, writer);
 		writer.finish();
+	}
+	
+	private PrefixMapping ensureRDFPrefix(PrefixMapping prefixes) {
+		// Some prefix already registered for the RDF namespace -- good enough
+		if (prefixes.getNsURIPrefix(RDF.getURI()) != null) return prefixes;
+		// rdf: is registered to something else -- give up
+		if (prefixes.getNsPrefixURI("rdf") == null) return prefixes;
+		// Register rdf:
+		PrefixMapping newPrefixes = new PrefixMappingImpl();
+		newPrefixes.setNsPrefixes(prefixes);
+		newPrefixes.setNsPrefix("rdf", RDF.getURI());
+		return newPrefixes;
 	}
 }
