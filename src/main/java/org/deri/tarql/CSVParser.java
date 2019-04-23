23e2fcc4d0909efd73abb11f1848a55c288c19a3
Plamen Tarkalanov <plamen.tarkalanov@ontotext.com>
2019-04-23 14:23:30 +0300

    fix indentation

--- src/main/java/org/deri/tarql/CSVParser.java
+++ src/main/java/org/deri/tarql/CSVParser.java
@@ -84,11 +84,9 @@ public class CSVParser implements ClosableIterator<Binding> {
 		*/
 
 		String PN_CHARS_BASE = "A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD";
-		String pattern =   PN_CHARS_BASE + "0-9\u00B7\u0300-\u036F\u203F-\u2040";
+		String pattern = PN_CHARS_BASE + "0-9\u00B7\u0300-\u036F\u203F-\u2040";
 
-        s = s.trim().replaceAll("[^" + pattern + "]", "_").replace(":", "");
-
-		//.replaceAll("_+", "_").replaceAll("^_|_$", ""); - suggest to clean-up the variable name
+		s = s.trim().replaceAll("[^" + pattern + "]", "_").replace(":", "");
 
 		if ("".equals(s))
 			return null;
